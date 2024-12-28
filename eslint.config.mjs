1789fefefd28c506aacbffa6a4642d157e23a81a
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2024-12-29 00:41:49 +0900

    ESLint&Prettierの設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tailwind from 'eslint-plugin-tailwindcss';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript']
+  }),
+  eslintConfigPrettier,
+  ...tailwind.configs['flat/recommended'],
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn'
+    }
+  }
 ];
 
 export default eslintConfig;
